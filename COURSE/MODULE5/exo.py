6a84932504a15bd9581427a84883e05aec5fbcfb
fatou1526 <95891257+fatou1526@users.noreply.github.com>
2022-08-25 15:19:08 +0000

    Create exo.py

---
+++ COURSE/MODULE5/exo.py
@@ -0,0 +1,8 @@
+from libraries.utils import Utils
+from libraries.csv import CsvFactory
+from libraries.json import JsonFactory
+from libraries.html import HtmlFactory
+if __name__ == '__main__':
+    print(Utils.divider())
+    print(JsonFactory.main())
+    print('\n')
