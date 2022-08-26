d68796cdffb287c6328c3e3b21bcc9d64111c731
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2022-08-26 00:36:52 +0000

    Definition et appel de la methode concatenener dans exo.py pour regrouper les donnees de  csv, json et html

--- COURSE/MODULE5/exo.py
+++ COURSE/MODULE5/exo.py
@@ -2,8 +2,22 @@ from libraries.utils import Utils
 from libraries.csv import CsvFactory
 from libraries.json import JsonFactory
 from libraries.html import HtmlFactory
+
+"""
+    4. Grace aux trois Factory, implémenter une méthode qui concatène tous les trois datas. C'est à dire:
+	- Celui de CsvFactory
+	- Celui de JsonFactory
+	- Celui de HtmlFactory
+"""
+def concatener(data1, data2, data3):
+    final_data = data1 + data2 + data3
+    return final_data
 if __name__ == '__main__':
     print(Utils.divider())
-    # Question 1 Recuperation des données html
+    # Question 1, 2 et 3: Recuperation et affichage des données html
     print(HtmlFactory.main())
+    print(Utils.divider())
+    # Question 4: Concaténation des bases de données Html, Csv et Json
+    global_data = concatener(HtmlFactory.main(),CsvFactory.main(),JsonFactory.main())
+    print(global_data)
     print('\n')
