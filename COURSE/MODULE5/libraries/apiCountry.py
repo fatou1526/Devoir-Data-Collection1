5c5c0db97702a12209b93650e5192eb0474bbbe0
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2022-08-26 16:22:01 +0000

    Suppressionde la requete de l'API et modification de la classe

--- COURSE/MODULE5/libraries/apiCountry.py
+++ COURSE/MODULE5/libraries/apiCountry.py
@@ -1,19 +1,12 @@
 from .utils import Utils
-import requests
-import json
 
-URL = 'https://restcountries.com/v2/all'
-countryData = requests.get(URL)
-countryData = json.loads(countryData.text[:])
-print(countryData)
-
-class CountryData(object):
+class CountryDonnees(object):
     @classmethod
     def addCountry(cls, data):
         def country_flag(rowData):
             countries = []
-            for pays in countryData:
-                country.append({'name':pays['name'], 'flag':pays['flags']['png']})
+            for pays in data:
+                countries.append({'name':pays['name'], 'flag':pays['flags']['png']})
                 rowData['country'] = countries[Utils.randomizeCountry(countries)]['name']
                 rowData['flag'] = countries[Utils.randomizeCountry(countries)]['flag']
                 return rowData
