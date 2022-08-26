f90466a265412bd11b0239ce75cf7bf3c6053391
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2022-08-26 01:08:14 +0000

    Import de CurrencyScrapper du fichier bceao.py et de la bibliothèque pandas, ajout de la colonne Devise dans le datframe global

--- COURSE/MODULE5/exo.py
+++ COURSE/MODULE5/exo.py
@@ -2,7 +2,8 @@ from libraries.utils import Utils
 from libraries.csv import CsvFactory
 from libraries.json import JsonFactory
 from libraries.html import HtmlFactory
-
+from libraries.bceao import CurrencyScrapper
+import pandas as pd
 """
     4. Grace aux trois Factory, implémenter une méthode qui concatène tous les trois datas. C'est à dire:
 	- Celui de CsvFactory
@@ -20,4 +21,18 @@ if __name__ == '__main__':
     # Question 4: Concaténation des bases de données Html, Csv et Json
     global_data = concatener(HtmlFactory.main(),CsvFactory.main(),JsonFactory.main())
     print(global_data)
-    print('\n')
+    
+    print(Utils.divider())
+    """5. Utiliser le lien de la BCEAO concernant les devises:
+	- Ajouter une nouvelle entrée dans la donnée globale
+	- Puis cette entrée doit contenir (Euro, Dollar, Yen)
+	- Attribuer de manière aléatoire ces Devises
+	- Grace aux données collecter via Scrapping du site de la BCEAO concernant les devises
+    """
+    print(CurrencyScrapper.makeCurrencyList())
+    print(Utils.divider())
+    global_data = pd.DataFrame(global_data)
+    global_data['Devise']=""
+    global_data['Devise'] = global_data['Devise'] \
+        .apply(lambda x: Utils.randomizeCurrency(['Euro', 'Dollar us', 'Yen japonais']))
+    print(global_data)
